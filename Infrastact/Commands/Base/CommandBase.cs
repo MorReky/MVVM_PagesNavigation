21c4d2142f22e1beab44ed61f70395fd26895350
MorReky <mormemir@yandex.ru>
2020-12-01 05:01:57 +0000

    Реализованы базовые классы для использования комманд

---
+++ Infrastact/Commands/Base/CommandBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_PagesNavigation.Infrastact.Commands.Base
+{
+    internal abstract class CommandBase : ICommand
+    {
+        /// <summary>
+        /// Событие изменения возможности выполнения Execute
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+        
+
+        public abstract void Execute(object parameter);
+    }
+}
